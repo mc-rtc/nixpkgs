989c47af4de6834dca0d9c299346d73f13b6fcc9
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-01-21 18:35:28 +0800

    Allow to choose whether to use mc-rtc with or without tvm

--- shell.nix
+++ shell.nix
@@ -1,5 +1,19 @@
-{ pkgs ? import <nixpkgs> { overlays = [ (import ./default.nix) ]; }
+{ pkgs ? import <nixpkgs> { overlays = [ (import ./default.nix) ]; },
+  with-tvm ? false
 }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [ cmake mc-rtc tvm ];
+pkgs.mkShell rec {
+  mc-rtc = if with-tvm then pkgs.mc-rtc-tvm else pkgs.mc-rtc;
+  buildInputs = builtins.trace mc-rtc.with-tvm [ pkgs.cmake mc-rtc ];
+  shellHook = ''
+    export TMP=/tmp
+    export TMPDIR=/tmp
+    export TEMP=/tmp
+    export TEMPDIR=/tmp
+    test_dir=$(cd test && pwd);
+    tmp_dir=$(mktemp -d)
+    cd $tmp_dir
+    cmake $test_dir -DCMAKE_BUILD_TYPE=Release
+    make
+    ./main
+  '';
 }
