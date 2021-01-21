76101669e6c3985a64d459ea809b2220e0e761a8
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-01-21 17:45:50 +0800

    Add a sample test project

---
+++ test/main.cpp
@@ -0,0 +1,37 @@
+#include <mc_rtc/logging.h>
+#include <mc_rtc/version.h>
+
+#include <mc_rbdyn/RobotLoader.h>
+
+#include <mc_control/mc_global_controller.h>
+
+#include <chrono>
+#include <thread>
+
+int main()
+{
+  mc_rtc::log::success("mc_rtc::version() {}", mc_rtc::version());
+
+  mc_control::MCGlobalController gc;
+
+  std::vector<double> initq;
+  initq.reserve(gc.robot().refJointOrder().size());
+  for(const auto & j : gc.robot().refJointOrder())
+  {
+    initq.push_back(gc.robot().mbc().q[gc.robot().jointIndexByName(j)][0]);
+  }
+  gc.init(initq);
+  gc.running = true;
+
+  using clock = typename std::conditional<std::chrono::high_resolution_clock::is_steady,
+                                          std::chrono::high_resolution_clock, std::chrono::steady_clock>::type;
+
+  while(gc.running)
+  {
+    auto start = clock::now();
+    gc.run();
+    std::this_thread::sleep_until(start + std::chrono::milliseconds(static_cast<int>(1000 * gc.timestep())));
+  }
+
+  return 0;
+}
