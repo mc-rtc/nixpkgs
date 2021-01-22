8c56d37c3358e0c3730a1d787da9521b935b6012
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-01-22 12:26:26 +0800

    [shell] Automatically exit (and cleanup) if the ticker is interrupted

--- shell.nix
+++ shell.nix
@@ -9,7 +9,18 @@ pkgs.mkShell rec {
     export TEMP=/tmp
     export TEMPDIR=/tmp
     test_dir=$(cd test && pwd);
-    tmp_dir=$(mktemp -d)
+    tmp_dir=$(mktemp -d /tmp/mc-rtc-nix.XXXXXXXXXXXX)
+    cleanup_build() {
+      cd $HOME
+      rm -rf $tmp_dir
+      return 0
+    }
+    cleanup_and_exit() {
+      cleanup_build
+      exit
+    }
+    trap cleanup_build EXIT
+    trap cleanup_and_exit SIGINT
     cd $tmp_dir
     cmake $test_dir -DCMAKE_BUILD_TYPE=Release
     make
