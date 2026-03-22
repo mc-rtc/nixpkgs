da8da56facb6685472a2a72a1261cb1affad8d8c
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-03-22 12:54:17 +0100

    feat(magnum): add support for Sdl2Application

--- pkgs/3rd-party/magnum/magnum.nix
+++ pkgs/3rd-party/magnum/magnum.nix
@@ -5,11 +5,12 @@
   libGL,
   libX11,
   openal,
-  glfw3,
+  glfw,
   SDL2,
   useLocal ? false,
   localWorkspace ? null,
   magnumWithGlfwApplication ? true,
+  magnumWithSDL2Application ? true,
   magnumWithAnyImageImporter ? true,
   magnumWithAnySceneImporter ? true,
   magnumWithObjImporter ? true,
@@ -33,8 +34,12 @@ stdenv.mkDerivation (finalAttrs: {
       fetchFromGitHub {
         owner = "mosra";
         repo = "magnum";
+        # up-to-date
         rev = "2a3acc0f22f42026c04553a73c1549e577c54e2f";
         hash = "sha256-C9WxwC02J21SMPR2wtVZfgFaCRuZceyIAEjg0oiWkH4=";
+        # orginal from mc-rtc-magnum
+        # rev = "440653dbb8815ba28fd20203acae711dbb8b14b4";
+        # hash = "sha256-65Pa7NqUMHJX80oLoJ+WpB9tj00XQSFjJIuBahrlZtg=";
       };
 
   nativeBuildInputs = [ cmake ];
@@ -42,14 +47,15 @@ stdenv.mkDerivation (finalAttrs: {
     corrade
     libGL
     openal
-    glfw3
-    SDL2
+    glfw
   ]
-  ++ lib.optionals magnumWithGlfwApplication [ libX11 ];
+  ++ lib.optionals magnumWithGlfwApplication [ libX11 ]
+  ++ lib.optionals magnumWithSDL2Application [ SDL2 ];
   # propagatedBuildInputs = finalAttrs.buildInputs;
 
   cmakeFlags = [
     "-DMAGNUM_WITH_GLFWAPPLICATION=${if magnumWithGlfwApplication then "ON" else "OFF"}"
+    "-DMAGNUM_WITH_SDL2APPLICATION=${if magnumWithGlfwApplication then "ON" else "OFF"}"
     "-DMAGNUM_WITH_ANYIMAGEIMPORTER=${if magnumWithAnyImageImporter then "ON" else "OFF"}"
     "-DMAGNUM_WITH_ANYSCENEIMPORTER=${if magnumWithAnySceneImporter then "ON" else "OFF"}"
     "-DMAGNUM_WITH_OBJIMPORTER=${if magnumWithObjImporter then "ON" else "OFF"}"
