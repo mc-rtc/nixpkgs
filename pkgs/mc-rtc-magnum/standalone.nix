82b05d6b29d6babc630edb43cc4bb28eaf712c5f
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-04-17 17:42:38 +0200

    magnum: update mc-rtc-magnum with ipc/tcp options

--- pkgs/mc-rtc-magnum/standalone.nix
+++ pkgs/mc-rtc-magnum/standalone.nix
@@ -2,13 +2,11 @@
   stdenv,
   lib,
   makeWrapper,
-  fetchgit,
+  fetchFromGitHub,
   cmake,
   mc-rtc-imgui,
   magnum-with-plugins,
   imguizmo,
-  useLocal ? false,
-  localWorkspace ? null,
 }:
 
 # TODO: modularize the build of mc_rtc-magnum instead of using submodules
@@ -16,36 +14,13 @@ stdenv.mkDerivation {
   pname = "mc-rtc-magnum";
   version = "main";
 
-  # main as of 2025-25-11
-  # FIXME: release mc-rtc-magnum
-  # src = fetchgit {
-  #   url = "https://github.com/mc-rtc/mc_rtc-magnum.git";
-  #   rev = "3b4ace180e43281be717255b2629c5eb0ec2ccbb";
-  #   sha256 = "sha256-6wwvyec7yfxjpa/njqsrw2k78kbsazy9+id+mp2je/8=";
-  #   fetchsubmodules = true;
-  # };
-  src =
-    if useLocal then
-      builtins.trace "Using local workspace for mc_rtc-magnum: ${localWorkspace}/mc_rtc-magnum-standalone"
-        (
-          builtins.path {
-            path = "${localWorkspace}/mc_rtc-magnum-standalone";
-            name = "mc_rtc-magnum-src";
-          }
-        )
-    else
-      fetchgit {
-        url = "https://github.com/mc-rtc/mc_rtc-magnum.git";
-        # NOTE
-        # topic/nix
-        # Use `nix` branch because it is hard to build against
-        # imgui/implot/imguizmo on non nix systems, and we need to keep
-        # supporting this: https://github.com/mc-rtc/mc_rtc-magnum/pull/4
-        # To be rebased on top of master on upgrade
-        rev = "97eb246eebda380b65b366e178a32107ad5a08d5";
-        sha256 = "sha256-Lek+JAQR6L2tgWXP1tsy077kVVh5roonuJqIDmWSNv4=";
-        fetchSubmodules = true;
-      };
+  # main on branch topic/nix
+  src = fetchFromGitHub {
+    owner = "mc-rtc";
+    repo = "mc_rtc-magnum";
+    rev = "734821ad335ad9b489e6db4ed60e248d5a02b9ca";
+    hash = "sha256-2cRQUHJv3PhLYcvy2/Q2JApAY7cwtzEB4xHzXqf5+5g=";
+  };
 
   nativeBuildInputs = [
     cmake
