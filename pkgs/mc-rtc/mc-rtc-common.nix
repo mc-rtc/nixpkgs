b58741c6123086a2b604601ff333518c6477f9e8
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-04-24 14:26:04 +0200

    fix(fmt): update mc-rtc version to include #528

--- pkgs/mc-rtc/mc-rtc-common.nix
+++ pkgs/mc-rtc/mc-rtc-common.nix
@@ -7,8 +7,8 @@ let
   src = fetchFromGitHub {
     owner = "jrl-umi3218";
     repo = "mc_rtc";
-    rev = "c193a3d46c6e8f9bbf5275e612f6ddf3f53ca6b2";
-    hash = "sha256-jrQc7LLAvb0BD8eTSdeqUKayz8yOru6wno3s7/sLQA4=";
+    rev = "d1bb28f9bfab85a04ce1ff103289676cfa5bf4fc";
+    hash = "sha256-a8Wh5Xhvs+vM7k0uEJzSDCSUOK58AJSRSJx3XyboHO0=";
   };
 in
 {
