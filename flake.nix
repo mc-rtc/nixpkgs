2271b5b82e3c6d6aee67dd47c655d5bc8c617a07
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-06-11 01:12:33 +0200

    feat(modules): rewrite module.nix with options

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     gepetto.url = "github:gepetto/nix";
     flake-parts.follows = "gepetto/flake-parts";
+    flakoboros.follows = "gepetto/flakoboros";
     nixpkgs.follows = "gepetto/nixpkgs";
     systems.follows = "gepetto/systems";
     jrl-cmakemodulesv2 = {
@@ -27,116 +28,21 @@
   outputs =
     inputs:
     let
-      mkModule =
-        arg:
-        let
-          attrs = if builtins.isAttrs arg then arg else { };
-        in
-        inputs.flake-parts.lib.importApply ./module.nix (
-          {
-            inherit (inputs) gepetto jrl-cmakemodulesv2;
-            lib = inputs.nixpkgs.lib;
-          }
-          // attrs
-        );
-
-      superbuildModule = inputs.flake-parts.lib.importApply ./modules/superbuild.nix ({
-        nixpkgs = inputs.nixpkgs;
-      });
-      flakeModule = mkModule { importPerSystem = false; };
-      flakeModuleCcache = mkModule {
-        importPerSystem = false;
-        enableCcacheOverlay = true;
+      flakeModule = inputs.flake-parts.lib.importApply ./module.nix {
+        inherit (inputs) gepetto;
       };
-      flakeModulePrivate = mkModule {
-        importPerSystem = false;
-        enablePrivateOverlay = true;
-      };
-      flakeModulePrivateCcache = mkModule {
-        importPerSystem = false;
-        enablePrivateOverlay = true;
-        enableCcacheOverlay = true;
-      };
-
-      mkFlakoboros =
-        {
-          localInputs,
-          localFlakeModule ? flakeModule,
-        }:
-        flakoborosModule:
-        inputs.flake-parts.lib.mkFlake { inputs = localInputs; } (args: {
-          systems = import inputs.systems;
-          imports = [
-            localFlakeModule
-            { flakoboros = flakoborosModule args; }
-          ];
-        });
     in
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } ({
-      # Default flake-parts module (public, with-ros)
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
       imports = [
-        (mkModule { importPerSystem = true; })
-        inputs.flake-parts.flakeModules.flakeModules
+        flakeModule
+        {
+          mc-rtc.importPerSystem = true;
+        }
       ];
 
       flake = {
-        flakeModules = {
-          default = flakeModule;
-          public = flakeModule;
-          public-ccache = flakeModuleCcache;
-          private = flakeModulePrivate;
-          private-ccache = flakeModulePrivateCcache;
-          superbuild = superbuildModule;
-        };
-        flakeModulePrivate = builtins.trace "WARNING: deprecated, use flakeModules.private or flakeModules.private-ccache instead" flakeModulePrivate;
-
-        # lib.mkFlakoboros
-        #   Usage: lib.mkFlakoboros localInputs module
-        #   Description: Creates a flake using the default flakeModule (public, with-ros).
-        #   Arguments:
-        #     - localInputs: The flake inputs set.
-        #     - flakoborosModule: The flake-parts module to use.
-        #                         See https://gepetto.github.io/flakoboros/index.html
-        lib.mkFlakoboros =
-          localInputs: flakoborosModule: mkFlakoboros { inherit localInputs; } flakoborosModule;
-
-        # lib.mkFlakoborosPrivate
-        #   Usage: lib.mkFlakoborosPrivate localInputs module
-        #   Description: Creates a flake using the private flakeModulePrivate (private, with-ros).
-        #   Arguments:
-        #     - localInputs: The flake inputs set.
-        #     - flakoborosModule: The flake-parts module to use.
-        #                         See https://gepetto.github.io/flakoboros/index.html
-        lib.mkFlakoborosPrivate =
-          localInputs: flakoborosModule:
-          mkFlakoboros {
-            inherit localInputs;
-            localFlakeModule = flakeModulePrivate;
-          } flakoborosModule;
-
-        # lib.mkFlakoborosCustom
-        #   Usage: lib.mkFlakoborosCustom localInputs localFlakeModule module
-        #   Description: Creates a flake using a custom flake module (pass arguments to mc-rtc's nixpkgs flake module).
-        #   Arguments:
-        #     - localInputs: The flake inputs set.
-        #     - localFlakeModule: The flake module to use.
-        #     - flakoborosModule: The flake-parts module to use.
-        #                         See https://gepetto.github.io/flakoboros/index.html
-        # Example:
-        # outputs = inputs:
-        #   inputs.mc-rtc-nix.lib.mkFlakoborosCustom
-        #     inputs
-        #     (flakeModule { with-ros = false; })
-        #     ({ lib, ... }: {
-        #       # your flakoboros module here
-        #       overrideAttrs.package = {
-        #         src = lib.cleanSource ./.;
-        #       }
-        #     })
-        lib.mkFlakoborosCustom =
-          localInputs: localFlakeModule: flakoborosModule:
-          mkFlakoboros { inherit localInputs localFlakeModule; } flakoborosModule;
+        inherit flakeModule;
 
         templates = {
           default = {
@@ -153,42 +59,5 @@
           };
         };
       };
-    })
-    // {
-      variants = {
-        default = inputs.flake-parts.lib.mkFlake { inherit inputs; } ({
-          systems = import inputs.systems;
-          imports = [ (mkModule { importPerSystem = true; }) ];
-        });
-        ccache = inputs.flake-parts.lib.mkFlake { inherit inputs; } ({
-          systems = import inputs.systems;
-          imports = [
-            (mkModule {
-              importPerSystem = true;
-              enableCcacheOverlay = true;
-            })
-          ];
-        });
-        private = inputs.flake-parts.lib.mkFlake { inherit inputs; } ({
-          systems = import inputs.systems;
-          imports = [
-            (mkModule {
-              importPerSystem = true;
-              enablePrivateOverlay = true;
-            })
-          ];
-        });
-        private-ccache = inputs.flake-parts.lib.mkFlake { inherit inputs; } ({
-          systems = import inputs.systems;
-          imports = [
-            (mkModule {
-              importPerSystem = true;
-              enablePrivateOverlay = true;
-              enableCcacheOverlay = true;
-            })
-          ];
-        });
-      };
     };
-
 }
--- module.nix
+++ module.nix
@@ -1,185 +1,202 @@
+# The importApply argument. Use this to reference things defined locally,
+# as opposed to the flake where this is imported.
+{ gepetto, ... }: # localFlake
+
+# Regular module arguments; self, inputs, etc all reference the final user flake,
+# where this module was imported.
 {
   lib,
-  gepetto,
-  jrl-cmakemodulesv2,
-  enablePrivateOverlay ? false,
-  enableCcacheOverlay ? false,
-  importPerSystem ? false,
-  with-ros ? true,
+  config,
+  inputs,
   ...
 }:
-{ ... }:
-let
-  # define overlays as a list set of name/overlay
-  rawOverlays = [
-    {
-      name = "mc-rtc-pkgs";
-      value = import ./overlay.nix { inherit lib with-ros; };
-    }
-  ]
-  ++ (lib.optional enablePrivateOverlay {
-    name = "mc-rtc-private";
-    value = import ./overlay-private.nix { inherit with-ros; };
-  })
-  ++ [
-    {
-      name = "jrl-cmakemodulesv2";
-      value = (
-        _final: prev: { jrl-cmakemodulesv2 = jrl-cmakemodulesv2.packages.${prev.system}.default; }
-      );
-    }
-  ]
-  ++ (lib.optional enableCcacheOverlay {
-    name = "mc-rtc-ccache";
-    value = import ./overlay-ccache.nix { };
-  });
-
-  flakeOverlays = builtins.listToAttrs (
-    map (o: {
-      inherit (o) name;
-      value = o.value;
-    }) rawOverlays
-  );
-  overlaysList = map (o: o.value) rawOverlays;
-  overlaysListTraced = builtins.trace "mc-rtc-nix: adding additional overlays: ${builtins.toString (map (o: o.name) rawOverlays)}" overlaysList;
-
-in
 {
-  imports = [ gepetto.flakeModule ];
-
-  config = {
-    flake.overlays = flakeOverlays;
-
-    flakoboros = {
-      extraPackages = [ "ninja" ];
-      overlays = overlaysListTraced;
-      nixpkgsConfig = {
-        permittedInsecurePackages = [
-          "openssl-1.1.1w"
-        ];
-      };
+  options.mc-rtc = {
+    enablePrivateOverlay = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
     };
-  }
-  // lib.optionalAttrs importPerSystem {
-    perSystem =
-      { inputs', pkgs, ... }:
-      {
-        packages =
-          inputs'.gepetto.packages
-          // {
-            # Main dependencies
-            inherit (pkgs)
-              eigen3-to-python
-              spacevecalg
-              rbdyn
-              sch-core
-              sch-core-python
-              tasks
-              tasks-qld
-              tvm
-              eigen-quadprog
-              eigen-qld
-              state-observation
-              mesh-sampling
-              eigen-fmt
-              ;
-
-            # mc-rtc
-            inherit (pkgs)
-              mc-rtc-data
-              mc-rtc
-              ;
-
-            # Main GUIs and applications
-            inherit (pkgs)
-              mc-rtc-magnum
-              mc-rtc-ticker
-              # Control interfaces
-              mc-franka
-              ;
-
-            # Main robots
-            inherit (pkgs)
-              mc-g1
-              mc-h1
-              mc-ur5e
-              mc-panda
-              mc-panda-lirmm
-              ;
-
-            # MuJoCo Robots
-            inherit (pkgs)
-              h1-mj-description
-              jvrc1-mj-description
-              g1-mj-description
-              ur5e-mj-description
-              env-mj-description
-              ;
-            # MuJoCo
-            inherit (pkgs) mc-mujoco mc-mujoco-full;
-
-            # Main superbuild configurations
-            inherit (pkgs) mc-rtc-superbuild mc-rtc-superbuild-full;
-
-            # Main controllers
-            inherit (pkgs) panda-prosthesis;
-
-            # Main plugins
-            inherit (pkgs) mc-force-shoe-plugin;
-
-            inherit (pkgs) sphinx-cmake;
-          }
-          // lib.optionalAttrs enablePrivateOverlay {
-            # Private robots
-            inherit (pkgs)
-              mc-hrp2
-              mc-hrp4
-              mc-hrp5-p
-              mc-rhps1
-              ;
-            inherit (pkgs) tasks-lssol;
-            # Hugo's demo
-            inherit (pkgs)
-              politopix
-              mc-dynamic-polytopes
-              dcm-vrptask
-              polytopeController
-              ;
-            # Superbuild configurations needing at least one private package
-            inherit (pkgs) mc-rtc-superbuild-private;
-          };
-
-        devShells =
-          inputs'.gepetto.devShells
-          // {
-            mc-rtc-superbuild-minimal = import ./shell.nix {
-              inherit pkgs;
-              with-ros = true;
-              mc-rtc-superbuild = pkgs.mc-rtc-superbuild-minimal;
-            };
-            mc-rtc-superbuild = import ./shell.nix {
-              inherit pkgs;
-              with-ros = true;
-              mc-rtc-superbuild = pkgs.mc-rtc-superbuild;
-            };
-            mc-rtc-superbuild-all-public-robots = import ./shell.nix {
-              inherit pkgs;
-              with-ros = true;
-              mc-rtc-superbuild = pkgs.mc-rtc-superbuild-all-public-robots;
-            };
-            mc-rtc-superbuild-full = import ./shell.nix {
-              inherit pkgs;
-              with-ros = true;
-              mc-rtc-superbuild = pkgs.mc-rtc-superbuild-full;
-            };
-          }
-          // lib.optionalAttrs enablePrivateOverlay {
-            mc-rtc-superbuild-private = import ../shell.nix {
-              inherit pkgs;
-              with-ros = true;
-              mc-rtc-superbuild = pkgs.mc-rtc-superbuild-private;
-            };
-          };
-      };
+    enableCcacheOverlay = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+    importPerSystem = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+    with-ros = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
   };
+
+  imports = [
+    gepetto.flakeModule
+    # ./modules/superbuild.nix
+  ];
+
+  config =
+    let
+      cfg = config.mc-rtc;
+
+      jrl-cmakemodulesv2 = inputs.jrl-cmakemodulesv2;
+
+      rawOverlays = [
+        {
+          name = "mc-rtc-pkgs";
+          value = import ./overlay.nix {
+            inherit lib;
+            with-ros = cfg.with-ros;
+          };
+        }
+      ]
+      ++ (lib.optional cfg.enablePrivateOverlay {
+        name = "mc-rtc-private";
+        value = import ./overlay-private.nix { with-ros = cfg.with-ros; };
+      })
+      ++ [
+        {
+          name = "jrl-cmakemodulesv2";
+          value = (
+            _final: prev: { jrl-cmakemodulesv2 = jrl-cmakemodulesv2.packages.${prev.system}.default; }
+          );
+        }
+      ]
+      ++ (lib.optional cfg.enableCcacheOverlay {
+        name = "mc-rtc-ccache";
+        value = import ./overlay-ccache.nix { };
+      });
+
+      flakeOverlays = builtins.listToAttrs (
+        map (o: {
+          inherit (o) name;
+          value = o.value;
+        }) rawOverlays
+      );
+
+      overlaysList = map (o: o.value) rawOverlays;
+      overlaysListTraced = builtins.trace "mc-rtc-nix: adding additional overlays: ${builtins.toString (map (o: o.name) rawOverlays)}" overlaysList;
+
+    in
+    {
+      flake.overlays = flakeOverlays;
+
+      flakoboros = {
+        extraPackages = [ "ninja" ];
+        overlays = overlaysListTraced;
+        nixpkgsConfig = {
+          permittedInsecurePackages = [ "openssl-1.1.1w" ];
+        };
+      };
+
+      perSystem = lib.mkIf cfg.importPerSystem (
+        { pkgs, ... }:
+        {
+          packages =
+            # inputs'.gepetto.packages
+            {
+            }
+            // {
+              # Main dependencies
+              inherit (pkgs)
+                eigen3-to-python
+                spacevecalg
+                rbdyn
+                sch-core
+                sch-core-python
+                tasks
+                tasks-qld
+                tvm
+                eigen-quadprog
+                eigen-qld
+                state-observation
+                mesh-sampling
+                eigen-fmt
+                ;
+
+              # mc-rtc
+              inherit (pkgs) mc-rtc-data mc-rtc;
+
+              # Main GUIs and applications
+              inherit (pkgs) mc-rtc-magnum mc-rtc-ticker mc-franka;
+
+              # Main robots
+              inherit (pkgs)
+                mc-g1
+                mc-h1
+                mc-ur5e
+                mc-panda
+                mc-panda-lirmm
+                ;
+
+              # MuJoCo Robots
+              inherit (pkgs)
+                h1-mj-description
+                jvrc1-mj-description
+                g1-mj-description
+                ur5e-mj-description
+                env-mj-description
+                ;
+
+              inherit (pkgs)
+                mc-mujoco
+                mc-mujoco-full
+                mc-rtc-superbuild
+                mc-rtc-superbuild-full
+                ;
+              inherit (pkgs) panda-prosthesis mc-force-shoe-plugin sphinx-cmake;
+            }
+            // lib.optionalAttrs cfg.enablePrivateOverlay {
+              inherit (pkgs)
+                mc-hrp2
+                mc-hrp4
+                mc-hrp5-p
+                mc-rhps1
+                tasks-lssol
+                ;
+              inherit (pkgs)
+                politopix
+                mc-dynamic-polytopes
+                dcm-vrptask
+                polytopeController
+                ;
+              inherit (pkgs) mc-rtc-superbuild-private;
+            };
+
+          devShells =
+            # inputs'.gepetto.devShells
+            {
+            }
+            // {
+              mc-rtc-superbuild-minimal = import ./shell.nix {
+                inherit pkgs;
+                with-ros = true;
+                mc-rtc-superbuild = pkgs.mc-rtc-superbuild-minimal;
+              };
+              mc-rtc-superbuild = import ./shell.nix {
+                inherit pkgs;
+                with-ros = true;
+                mc-rtc-superbuild = pkgs.mc-rtc-superbuild;
+              };
+              mc-rtc-superbuild-all-public-robots = import ./shell.nix {
+                inherit pkgs;
+                with-ros = true;
+                mc-rtc-superbuild = pkgs.mc-rtc-superbuild-all-public-robots;
+              };
+              mc-rtc-superbuild-full = import ./shell.nix {
+                inherit pkgs;
+                with-ros = true;
+                mc-rtc-superbuild = pkgs.mc-rtc-superbuild-full;
+              };
+            }
+            // lib.optionalAttrs cfg.enablePrivateOverlay {
+              mc-rtc-superbuild-private = import ../shell.nix {
+                inherit pkgs;
+                with-ros = true;
+                mc-rtc-superbuild = pkgs.mc-rtc-superbuild-private;
+              };
+            };
+        }
+      );
+    };
 }
