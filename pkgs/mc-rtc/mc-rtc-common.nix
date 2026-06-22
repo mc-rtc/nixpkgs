b6ac9b80218668011d47c7e5d50ffba59d350c63
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-06-22 13:36:58 +0200

    mc-rtc: update to merged #552 (macos build fix)

--- pkgs/mc-rtc/mc-rtc-common.nix
+++ pkgs/mc-rtc/mc-rtc-common.nix
@@ -5,17 +5,11 @@
 let
   version = "2.14.1";
   # TODO: Release
-  # src = fetchFromGitHub {
-  #   owner = "jrl-umi3218";
-  #   repo = "mc_rtc";
-  #   rev = "cb1850732afa034ef99e42c6e1158a1a0f3bbb0c";
-  #   hash = "sha256-rmIRRFqiToZTdGPt24VYYGk+UsdkRRrixR4F3heeH0s=";
-  # };
   src = fetchFromGitHub {
     owner = "jrl-umi3218";
     repo = "mc_rtc";
-    rev = "70e8a93d6657dc5bc46aff6f7271d8953e4a2b22";
-    hash = "sha256-TuyiflhG44zsABmewrpihGv1sM8vs/GECf84l6N41jE=";
+    rev = "c09df00517a18f53236ce4b66599e9e6b68e3c08";
+    hash = "sha256-HpgoiO8cvYeocr+t7eYN3imb7Oi5C+EYlEZ0GQzu4IA=";
   };
 in
 {
