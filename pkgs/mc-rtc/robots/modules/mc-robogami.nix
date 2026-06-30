478cdfeb5f0c569b6425d5169002e91504518b3d
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-06-30 18:07:19 +0200

    robots(robogami): remove boost dependency

--- pkgs/mc-rtc/robots/modules/mc-robogami.nix
+++ pkgs/mc-rtc/robots/modules/mc-robogami.nix
@@ -20,11 +20,17 @@ stdenv.mkDerivation {
   version = "0.0.0";
 
   # TODO: release
+  # src = fetchFromGitHub {
+  #   owner = "anastasiabolotnikova";
+  #   repo = "mc_robogami";
+  #   rev = "3d026f529dead658d478e729857e031209e96ed8";
+  #   hash = "sha256-8TRJi0ZLyEBxz8/iFXGt+qBu+gIs0qXyDV0Hmz1CT4I=";
+  # };
   src = fetchFromGitHub {
-    owner = "anastasiabolotnikova";
+    owner = "arntanguy";
     repo = "mc_robogami";
-    rev = "3d026f529dead658d478e729857e031209e96ed8";
-    hash = "sha256-8TRJi0ZLyEBxz8/iFXGt+qBu+gIs0qXyDV0Hmz1CT4I=";
+    rev = "2d9ce185009a17ecf7e9fa85034ec73f67f0f776";
+    hash = "sha256-W9ACYU9dyPfCCDJRl99RtCx4gnaNlrmDuJayY8PcuYk=";
   };
 
   nativeBuildInputs = [ cmake ];
