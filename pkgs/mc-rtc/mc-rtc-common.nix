dba25e443d7d300cb135177e695ebddb779f8804
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-07-06 09:14:16 +0200

    fix(mc-rtc): fix qhull convex generation issue

--- pkgs/mc-rtc/mc-rtc-common.nix
+++ pkgs/mc-rtc/mc-rtc-common.nix
@@ -3,13 +3,13 @@
 }:
 
 let
-  version = "2.14.1";
+  version = "2.15-unreleased";
   # TODO: Release
   src = fetchFromGitHub {
     owner = "jrl-umi3218";
     repo = "mc_rtc";
-    rev = "c09df00517a18f53236ce4b66599e9e6b68e3c08";
-    hash = "sha256-HpgoiO8cvYeocr+t7eYN3imb7Oi5C+EYlEZ0GQzu4IA=";
+    rev = "44829097027038747ee2be3a223fef1e27936c0b";
+    hash = "sha256-Krcey6ubiX0CQKJDLUgZwCCGiTpF6CGqTO9a9lWmTqs=";
   };
 in
 {
